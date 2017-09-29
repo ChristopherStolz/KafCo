117f94d7d61dfe9ac184d7a1551e7eb439001bd3
ChristopherStolz <ChristopherStolz@github.com>
2017-09-29 15:47:56 -0400

    Enabled routing. Still working on session variables.

--- src/main/java/edu/nyit/CSCI455/MeterProject/WelcomeController.java
+++ src/main/java/edu/nyit/CSCI455/MeterProject/WelcomeController.java
@@ -1,31 +1,17 @@
 package edu.nyit.CSCI455.MeterProject;
 
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class WelcomeController {
-	
 	@Value("${welcome.message:test}")
 	private String message = "Hello, World!";
-	
-	@RequestMapping("/Welcome")
-	public String welcome(Map<String, Object> model){
-		model.put("message", this.message);
+	@RequestMapping("/welcome")
+	public String welcome(ModelAndView modelAndView){
+		modelAndView.addObject("message", this.message);
 		return "welcome";
-	}
-	
-	@RequestMapping("/")
-	public String all(Map<String, Object> model){
-		throw new RuntimeException("Not Found");
-	}
-	@RequestMapping("/foo")
-	public String foo(Map<String, Object> model) {
-		throw new RuntimeException("Foo");
-	}
-	
+	}	
 }
