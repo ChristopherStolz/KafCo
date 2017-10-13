b3598a6421bce8179846dc919e5c63978e000e4d
ChristopherStolz <cstolz@gmail.com>
2017-10-13 22:16:37 +0800

    Changed serial connector. Switching to windows workspace for comm port development.

--- src/main/java/edu/nyit/CSCI455/MeterProject/Client/Meter.java
+++ src/main/java/edu/nyit/CSCI455/MeterProject/Client/Meter.java
@@ -1,19 +1,33 @@
 package edu.nyit.CSCI455.MeterProject.Client;
 
 import edu.nyit.CSCI455.MeterProject.Data.*;
+
+import jssc.SerialPort;
+import jssc.SerialPortEvent;
+import jssc.SerialPortEventListener;
+import jssc.SerialPortException;
+import jssc.SerialPortList;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.util.Date;
 
-import com.fazecast.jSerialComm.SerialPort;
+
 public class Meter {
+	
+	private final char STX = (char)2;
+	private final char ETX = (char)3;
+	private final char ZERO = (char)48;
 	
 	private InputStream iStream;
 	private OutputStream oStream;
-	private SerialPort serialPort;
+	private PrintWriter oWriter;
+	private BufferedReader iReader;
+	private SerialPort serialPort;	
 	private byte[] readBuffer;
 	private DataRun myData;
 	private int timeOffset;
@@ -24,20 +38,34 @@ public class Meter {
 	}
 	
 	public Meter(String meterName, int timeOffset) {
-		SerialPort[] temp = SerialPort.getCommPorts();
-		for (int i = 0; i < temp.length; i ++){
-			System.out.println(temp[i].getSystemPortName());
+		String[] portNames = SerialPortList.getPortNames();
+		
+		if (portNames.length == 0){
+			System.out.println("No serial device connected.");
 		}
-		serialPort = SerialPort.getCommPorts()[0];
+		
+		serialPort = new SerialPort(portNames[0]);
+		try{
+			
 		serialPort.openPort();
-		System.out.println(serialPort.isOpen() + "");
-		serialPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING | 
-				SerialPort.TIMEOUT_WRITE_SEMI_BLOCKING, 100, 100);
-		serialPort.setBaudRate(9600);
-		iStream = serialPort.getInputStream();
-		oStream = serialPort.getOutputStream();
+		
+		serialPort.setParams(serialPort.BAUDRATE_9600,
+							serialPort.DATABITS_8,
+							serialPort.STOPBITS_1,
+							serialPort.PARITY_NONE);
+		
+		serialPort.setFlowControlMode(serialPort.FLOWCONTROL_RTSCTS_IN |
+									  serialPort.FLOWCONTROL_RTSCTS_OUT);
+		
+		serialPort.addEventListener(new PortReader(), SerialPort.MASK_RXCHAR);
+		} catch (SerialPortException e) {
+			System.out.println(e.getStackTrace().toString());
+		}
+		//iStream = serialPort.getInputStream();
+		//oWriter = new PrintWriter(serialPort.getOutputStream());
+		//iReader = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
 		myName = meterName;
-		readBuffer = new byte[100];
+		readBuffer = new byte[8];
 		myData = new DataRun(myName, new Date().toString());
 		this.timeOffset = timeOffset;
 	}
@@ -47,14 +75,22 @@ public class Meter {
 		 * TODO: Define readThread 
 		 */
 		try{
-			oStream.write((byte)0);
-			int stream = iStream.read(readBuffer);
-			System.out.println(readBuffer);
-			
+			//System.out.println(serialPort.isOpen() + "");
+			//oWriter.write(STX + ZERO + ETX);
+			//oWriter.flush();
+			//int read = serialPort.readBytes(readBuffer, 8);
+			//String read = iReader.readLine();
+			//System.out.println(readBuffer + " test");
+			System.out.print(STX + "," + ZERO + "," + ETX);
+			serialPort.writeString(STX + "," + ZERO + "," + ETX);
 		} catch (Exception e){
 			e.printStackTrace();
 		}
-		serialPort.closePort();
+		try{
+			serialPort.closePort();
+		}catch (SerialPortException e){
+			System.out.println(e.getStackTrace().toString());
+		}
 		String[] outData = {"0", "" + readBuffer};
 		//myData.writeData(outData);
 /*		serialPort.openPort();
@@ -84,4 +120,20 @@ public class Meter {
 	public int getTimeOffset (){
 		return timeOffset;
 	}
+	
+	private class PortReader implements SerialPortEventListener {
+		@Override
+		public void serialEvent(SerialPortEvent event){
+			if (event.isRXCHAR() && event.getEventValue() > 0) {
+				try{
+					String receivedData = serialPort.readString(event.getEventValue());
+					System.out.println("Received response: " + receivedData);
+				}catch(SerialPortException e){
+					System.out.println("Error in receiving string " + e);
+				}
+			}
+		}
+	}
 }
+
+
